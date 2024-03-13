acd9f3a7e45451abee2e11c29bbf05e0ef8ce0d8
kairisflorian <kairisflorian@gmail.com>
2024-03-13 23:51:01 +0100

    Travail sur l'historique de transaction

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="container">
         <Balance />
         <IncomeExpenses />
-        <TransactionList />
+        <TransactionList :transactions="transactions" />
         <AddTransaction />
     </div>
 </template>
@@ -14,4 +14,13 @@ import Balance from "./components/Balance.vue";
 import IncomeExpenses from "./components/IncomeExpenses.vue";
 import TransactionList from "./components/TransactionList.vue";
 import AddTransaction from "./components/AddTransaction.vue";
+
+import { ref } from "vue";
+
+const transactions = ref([
+    { id: 1, text: "Fleurs", amount: -19.99 },
+    { id: 2, text: "Salaire", amount: 299.99 },
+    { id: 3, text: "Livre", amount: -10 },
+    { id: 4, text: "Caméra", amount: 150 },
+]);
 </script>
--- src/components/TransactionList.vue
+++ src/components/TransactionList.vue
@@ -1,15 +1,25 @@
 <template>
     <h3>Historique</h3>
     <ul id="list" class="list">
-        <li class="minus">
-            Courses
-            <span>-$400</span>
-            <button class="delete-btn">x</button>
-        </li>
-        <li class="plus">
-            Cheque
-            <span>$800</span>
+        <li
+            v-for="transaction in transactions"
+            :key="transaction.id"
+            :class="transaction.amount < 0 ? 'minus' : 'plus'"
+        >
+            {{ transaction.text }}
+            <span>
+                {{ transaction.amount }}
+            </span>
             <button class="delete-btn">x</button>
         </li>
     </ul>
 </template>
+
+<script setup>
+const props = defineProps({
+    transactions: {
+        type: Array,
+        required: true,
+    },
+});
+</script>
