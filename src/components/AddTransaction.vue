7a5882e698253c44b81687986e9e06c701e2c88f
kairisflorian <kairisflorian@gmail.com>
2024-03-18 14:55:07 +0100

    Ajout d'un systeme de notification avec vue-toastification

--- src/components/AddTransaction.vue
+++ src/components/AddTransaction.vue
@@ -1,9 +1,14 @@
 <template>
     <h3>Ajouter une nouvelle transaction</h3>
-    <form id="form">
+    <form id="form" @submit.prevent="onSubmit">
         <div class="form-control">
             <label for="text">Texte</label>
-            <input type="text" id="text" placeholder="Entrez un texte..." />
+            <input
+                type="text"
+                id="text"
+                placeholder="Entrez un texte..."
+                v-model="text"
+            />
         </div>
         <div class="form-control">
             <label for="montant">
@@ -11,11 +16,32 @@
                 (négatif - dépense, positif - revenu)
             </label>
             <input
-                type="number"
+                type="text"
                 id="amount"
                 placeholder="Entrez le montant..."
+                v-model="amount"
             />
         </div>
         <button class="btn">Ajouter la transaction</button>
     </form>
 </template>
+
+<script setup>
+import { ref } from "vue";
+import { useToast } from "vue-toastification";
+
+const text = ref("");
+const amount = ref("");
+
+const toast = useToast();
+
+const onSubmit = () => {
+    if (!text.value || !amount.value) {
+        toast.error("Les deux champs doivent être remplis.");
+        return;
+    }
+    console.log(text.value, amount.value);
+    text.value = "";
+    amount.value = "";
+};
+</script>
